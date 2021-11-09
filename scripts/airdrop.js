3dcc29c9199b681091ca12937bd47aa2715f73dd
jordang7 <77jgreenberg77@gmail.com>
2021-11-09 15:39:41 -0500

    cleanup

--- scripts/airdrop.js
+++ scripts/airdrop.js
@@ -7,22 +7,15 @@ async function main() {
   console.log("Account balance:", await ethers.utils.formatEther(weiAmount));
   const token = await hre.ethers.getContractAt("NYMets", CONTRACT_ADDRESS);
 
-  await token.transfer(
+  const recipients = [
     "0x0f729f69699fe3c6736d0e8974c7d4af8f0b1ce4",
-    ethers.utils.parseEther("9")
-  );
-  await token.transfer(
     "0x1eb0ba41cca2fe899ab238345e811d1f4be30ffb",
-    ethers.utils.parseEther("9")
-  );
-  await token.transfer(
     "0x4ac0184cbefb6971534c2fd7871c3f0ff4f2974d",
-    ethers.utils.parseEther("9")
-  );
-  await token.transfer(
     "0xa6b5885319f62dfb6df839fe1619995b35d92268",
-    ethers.utils.parseEther("9")
-  );
+  ];
+  for (addr of recipients) {
+    await token.transfer(addr, ethers.utils.parseEther("9"));
+  }
 }
 
 main()
